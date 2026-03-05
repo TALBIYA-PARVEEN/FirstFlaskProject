7127db28ddef704b03dbdfe7dc0e2ab498032777
TALBIYA-PARVEEN <2k24.cs1q.2413756@gmail.com>
2026-03-05 23:39:58 +0530

    final

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get("SECRET_KEY", "iit_madras_mad1_project_development-secret-key")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///careernest.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///careernest.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
