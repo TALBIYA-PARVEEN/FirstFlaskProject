98ce31aad223e735d11dd751367d981105bcd233
TALBIYA-PARVEEN <2k24.cs1q.2413756@gmail.com>
2026-03-08 15:37:58 +0530

    submit

--- generate_hash.py
+++
@@ -1,8 +0,0 @@
-from flask_bcrypt import Bcrypt
-from flask import Flask
-
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-
-hashed_password = bcrypt.generate_password_hash("1234567").decode('utf-8')
-print(hashed_password)
